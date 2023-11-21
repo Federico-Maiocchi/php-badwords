3a8ebd7e7ea7272ca91256ff116d3869330ebca5
Federico-Maiocchi <maiocchifederico@gmail.com>
2023-11-21 17:09:21 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BadWords</title>
+</head>
+<body>
+    <h1>Prova</h1>
+    <form action="result.php" method='POST'>
+        <input type="text" name="text_bad">
+        <input type="submit" value="Invia">
+    </form>
+</body>
+</html>
